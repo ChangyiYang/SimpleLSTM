907585a70b85ad80e3762a44a3072487dd9542ed
vaibhavippili <vaibhavippili@berkeley.edu>
2023-04-24 18:03:13 -0700

    removed batch norm

--- NeuralNets.py
+++ NeuralNets.py
@@ -4,40 +4,40 @@ import torch.nn as nn
 import torch
 
 
-# class SimpleLSTM(nn.Module):
-#     '''
-#     The simplest neural nets
-#     The structure likes this:
+class SimpleLSTM(nn.Module):
+    '''
+    The simplest neural nets
+    The structure likes this:
 
-#     LSTM layers -> Drop out layer -> Linear layer
+    LSTM layers -> Drop out layer -> Linear layer
     
     
-#     '''
-#     def __init__(self, input_dim = 150, hidden_dim = 64, output_dim =1, lstm_nums_layer =2 , dropout = 0.1):
-#         super().__init__()
-#         self.input_dim = input_dim
-#         self.hidden_dim = hidden_dim
-#         self.output_dim = output_dim
+    '''
+    def __init__(self, input_dim = 150, hidden_dim = 64, output_dim =1, lstm_nums_layer =2 , dropout = 0.1):
+        super().__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
         
         
-#         self.LSTM = nn.LSTM(input_dim, hidden_dim, lstm_nums_layer, batch_first = True)
-#         self.dropout = nn.Dropout(dropout)
+        self.LSTM = nn.LSTM(input_dim, hidden_dim, lstm_nums_layer, batch_first = True)
+        self.dropout = nn.Dropout(dropout)
         
-#         self.hidden_to_output = nn.Linear(hidden_dim, output_dim)
+        self.hidden_to_output = nn.Linear(hidden_dim, output_dim)
 
-#         self.to(torch.double)
+        self.to(torch.double)
         
-#     def forward(self, input):
-#         # print(input.shape)
+    def forward(self, input):
+        # print(input.shape)
         
-#         hidden_state, _ = self.LSTM(input)
+        hidden_state, _ = self.LSTM(input)
         
-#         # print(hidden_state.shape)
-#         output = self.dropout(hidden_state)
-#         output = self.hidden_to_output(output)
+        # print(hidden_state.shape)
+        output = self.dropout(hidden_state)
+        output = self.hidden_to_output(output)
         
         
-#         return output
+        return output
 
 
 # class SimpleLSTM(nn.Module):
@@ -79,40 +79,40 @@ import torch
 import torch
 import torch.nn as nn
 
-class SimpleLSTM(nn.Module):
-    '''
-    The simplest neural nets
-    The structure likes this:
+# class SimpleLSTM(nn.Module):
+#     '''
+#     The simplest neural nets
+#     The structure likes this:
 
-    BatchNorm -> LSTM layers -> Drop out layer -> Linear layer
+#     BatchNorm -> LSTM layers -> Drop out layer -> Linear layer
 
-    '''
-    def __init__(self, input_dim = 150, hidden_dim = 64, output_dim = 1, lstm_nums_layer = 2, dropout = 0.1):
-        super().__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
+#     '''
+#     def __init__(self, input_dim = 150, hidden_dim = 64, output_dim = 1, lstm_nums_layer = 2, dropout = 0.1):
+#         super().__init__()
+#         self.input_dim = input_dim
+#         self.hidden_dim = hidden_dim
+#         self.output_dim = output_dim
 
-        self.batch_norm = nn.BatchNorm1d(input_dim)
-        self.LSTM = nn.LSTM(input_dim, hidden_dim, lstm_nums_layer, batch_first = True)
-        self.dropout = nn.Dropout(dropout)
+#         self.batch_norm = nn.BatchNorm1d(input_dim)
+#         self.LSTM = nn.LSTM(input_dim, hidden_dim, lstm_nums_layer, batch_first = True)
+#         self.dropout = nn.Dropout(dropout)
 
-        self.hidden_to_output = nn.Linear(hidden_dim, output_dim)
+#         self.hidden_to_output = nn.Linear(hidden_dim, output_dim)
 
-        self.to(torch.double)
+#         self.to(torch.double)
 
-    def forward(self, input):
-        # print(input.shape)
+#     def forward(self, input):
+#         # print(input.shape)
 
-        # Permute the input tensor to apply batch normalization along the time dimension
-        input = input.permute(0, 2, 1)
-        input = self.batch_norm(input)
-        input = input.permute(0, 2, 1)
+#         # Permute the input tensor to apply batch normalization along the time dimension
+#         input = input.permute(0, 2, 1)
+#         input = self.batch_norm(input)
+#         input = input.permute(0, 2, 1)
 
-        hidden_state, _ = self.LSTM(input)
+#         hidden_state, _ = self.LSTM(input)
 
-        # print(hidden_state.shape)
-        output = self.dropout(hidden_state)
-        output = self.hidden_to_output(output)
+#         # print(hidden_state.shape)
+#         output = self.dropout(hidden_state)
+#         output = self.hidden_to_output(output)
 
-        return output
+#         return output
