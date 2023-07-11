0e73253730ec4ec4164fa79386d67eae5802ce68
ChangyiYang <1183533559@qq.com>
2023-07-12 02:08:44 +0800

    7.12 update

--- concentrate.py
+++ concentrate.py
@@ -1,21 +1,24 @@
 import os
 import pandas as pd
 
-def merge_csv_files(folder_path, output_path):
-    # Get the paths of all CSV files in the folder
-    csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+def merge_csv_files(folder_path, output_path, prefix = '' ,files=None):
 
-    print(csv_files)
+    files = [ prefix + x  for x in files]
+    # Get the paths of all CSV files in the folder
+    if files is None:
+        files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+    print(files)
     
-    if not csv_files:
+    if not files:
         print("No CSV files found in the folder.")
         return
     
     # Read the first CSV file and create the merged dataframe
-    merged_data = pd.read_csv(os.path.join(folder_path, csv_files[0]))
+    merged_data = pd.read_csv(os.path.join(folder_path, files[0]))
     
     # Loop through the remaining CSV files and merge them into the dataframe
-    for file in csv_files[1:]:
+    for file in files[1:]:
         file_path = os.path.join(folder_path, file)
         data = pd.read_csv(file_path)
         merged_data = pd.concat([merged_data, data], ignore_index=True)
@@ -25,8 +28,15 @@ def merge_csv_files(folder_path, output_path):
     print("CSV files merged successfully! Saved at:", output_path)
 
 # Test
-name = "Cs"
-folder_path = f'./Raw Data/Add Xe/{name}'  # Replace with your folder path
-output_path = f'./Raw Data/Add Xe/{name}.csv'  # Replace with your output path and filename
 
-merge_csv_files(folder_path, output_path)
+name = 'Xe'
+folder_path = f'./Raw Data/Add Xe/{name}'  # Replace with your folder path
+output_path = f'./Raw Data/Add Xe/{name}_testing.csv'  # Replace with your output path and filename
+prefix = 'Xe135_'
+
+# files = ['P0.6T1', 'P0.8T1', 'P1T0.9', 'P1T1.1']  # Replace with your list of file names
+files = ['easy', 'hard']
+
+files = [file + '.csv' for file in files]
+
+merge_csv_files(folder_path, output_path, prefix,files)
